bb22391ca78f5a20540b713b407f7b9ee4d438ff
Blissmal <malutibethuel@gmail.com>
2025-02-10 00:53:47 +0300

    Added code images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Categories } from '../assets/Data'
 
 const Home = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+            <div>
+                <div>
+                    SHOP BY CATEGORIES
+                </div>
+                <ul>
+                    {
+                        Categories.map((category, index) => (
+                            <li key={index}>
+                                <div></div>
+                                {category}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+            <div>
+                    <img src="" alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
