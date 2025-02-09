ca7dd19e7929ba65c0670283eec523236ec9c52d
Blissmal <malutibethuel@gmail.com>
2025-02-10 00:43:23 +0300

    Complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,16 @@ const Navbar = () => {
                 </form>
             </div>
             <div className='flex items-center space-x-4'>
-                <Link to="/cart"><FaShoppingCart /></Link>
+                <Link to="/cart"><FaShoppingCart className='text-lg'/></Link>
                 <button className='hidden md:block'>Login | Register</button>
                 <button className='block md:hidden'><FaUser /></button>
             </div>
+        </div>
+        <div className='flex items-center justify-center space-x-10 py-4 text-sm font-bold'>
+            <Link to='/' className='hover:underline'>Home</Link>
+            <Link to='/' className='hover:underline'>Shop</Link>
+            <Link to='/' className='hover:underline'>Contact</Link>
+            <Link to='/' className='hover:underline'>About</Link>
         </div>
     </nav>
   )
