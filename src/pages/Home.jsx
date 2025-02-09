228e248010277760955d24681e98a58f70cb095d
Blissmal <malutibethuel@gmail.com>
2025-02-10 01:04:20 +0300

    Left side hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Categories } from '../assets/Data'
+import React from "react";
+import { Categories } from "../assets/Data";
+import HeroImg from "../assets/images/hero-page.png";
 
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
+      <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
+        <div className="w-full md:w-3/12">
+          <div className="bg-red-600 text-white text-xs font-bold px-2 py-2.5">SHOP BY CATEGORIES</div>
+          <ul className="space-y-4 bg-gray-100 p-3 border">
+            {Categories.map((category, index) => (
+              <li key={index} className="flex items-center text-sm font-medium">
+                <div className="w-2 h-2 border bordr-red-500 rounded-full mr-2"></div>
+                {category}
+              </li>
+            ))}
+          </ul>
+        </div>
         <div>
-            <div>
-                <div>
-                    SHOP BY CATEGORIES
-                </div>
-                <ul>
-                    {
-                        Categories.map((category, index) => (
-                            <li key={index}>
-                                <div></div>
-                                {category}
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
-            <div>
-                    <img src="" alt="" />
-            </div>
+          <img src={HeroImg} alt="" />
+          <div>
+            <p>Bliss | eSHop</p>
+            <h2>WELCOME TO ESHOP</h2>
+            <p>MILLIONS+ PRODUCTS</p>
+            <button>SHOP</button>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
