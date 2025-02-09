f4f9581eab39ed1da0e7e0e89d9127786ea84789
Blissmal <malutibethuel@gmail.com>
2025-02-10 00:37:53 +0300

    Tailwind styling for nav section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { FaSearch, FaShoppingCart, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='bg-white shadow-md'>
+        <div className='container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center'>
+            <div className='text-lg font-bold'>
+                <Link to="/"></Link>e-Shop
+            </div>
+            <div className='relative flex-1 mx-4 '>
+                <form action="">
+                    <input className='w-full border py-2 px-4' type="text" placeholder='Search product' />
+                    <FaSearch className='absolute top-3 right-3 text-red-500'/>
+                </form>
+            </div>
+            <div>
+                <Link to="/cart"><FaShoppingCart /></Link>
+                <button>Login | Register</button>
+                <button><FaUser /></button>
+            </div>
+        </div>
+    </nav>
   )
 }
 
