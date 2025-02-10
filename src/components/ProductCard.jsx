75f06da6ea6df09aa057b039446166766964adfd
Blissmal <malutibethuel@gmail.com>
2025-02-11 01:05:52 +0300

    Updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -22,7 +22,7 @@ const ProductCard = ({product}) => {
         <FaStar className='text-yellow-500'/>
         <FaStar className='text-yellow-500'/>
       </div>
-      <div className='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all' onClick={() => handleAddToCart(event, product)}>
+      <div className='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all' onClick={(e) => handleAddToCart(e, product)}>
         <span className='group-hover:hidden'>+</span>
         <span className='hidden group-hover:block'>Add To Cart</span>
       </div>
