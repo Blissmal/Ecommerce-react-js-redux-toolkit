b8cdb154e6ae6cb23e87afb8529fc5f42bb4d0bc
Blissmal <malutibethuel@gmail.com>
2025-02-10 13:16:15 +0300

    Products display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     const products = useSelector(state => state.product)
     useEffect(() => {
         dispatch(setProducts(mockData))
-    })
+    }, [])
   return (
     <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
       <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
