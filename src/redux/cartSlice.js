4215740e8adbe7e10ddaa370fd767ae2748f4ce3
Blissmal <malutibethuel@gmail.com>
2025-02-10 14:37:54 +0300

    Added cart functionality

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -2,13 +2,31 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     products: [],
+    totalQuantity: 0,
+    totalPrice: 0,
 }
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        
+        addToCart(state, action) {
+            const newItem = action.payload;
+            const itemIndex = state.products.find(item => item.id === newItem.id) //finding index
+            if (itemIndex){
+                itemIndex.quantity ++;
+                itemIndex.totalPrice += newItem.price
+            } else {
+                state.products.push({
+                    id: newItem.id,
+                    name: newItem.name,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    image: newItem.image
+                })
+            }
+        }
     },
 })
 
