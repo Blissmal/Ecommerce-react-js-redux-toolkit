adc581ef3449d04f174e6aa56c33dcb5ae66a150
Blissmal <malutibethuel@gmail.com>
2025-02-11 01:38:45 +0300

    Added more tailwind classes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -13,9 +13,9 @@ const Cart = () => {
                 <h3 className='text-2xl font-semibold mb-4'>SHOPPING CART</h3>
                 <div className='flex flex-col md:flex-row justify-between space-x-10 mt-8'>
                     <div className='md:w-2/3'>
-                        <div className='flex justify-between border-b items-center b-4'>
+                        <div className='flex justify-between border-b items-center mb-4 text-xs font-bold'>
                             <p>PRODUCTS</p>
-                            <div>
+                            <div className='flex space-x-8'>
                                 <p>PRICE</p>
                                 <p>QUANTITY</p>
                                 <p>SUBTOTAL</p>
@@ -25,22 +25,22 @@ const Cart = () => {
                         <div>
                             {
                                 cart.products.map((product) => (
-                                    <div>
-                                        <div>
-                                            <img src={product.image} alt="" />
-                                            <div>
-                                                <h3>{product.name}</h3>
+                                    <div key={product.id} className='flex items-center justify-between p-3 border-b'>
+                                        <div className='md:flex items-center space-x-4'>
+                                            <img src={product.image} alt="" className='w-16 h-16 object-contain rounded'/>
+                                            <div className='flex-1 ml-4'>
+                                                <h3 className='text-lg font-semibold'>{product.name}</h3>
                                             </div>
                                         </div>
-                                        <div>
+                                        <div className='flex space-x-12 items-center'>
                                             <p>{product.price}</p>
-                                            <div className='flex'>
-                                                <button>-</button>
-                                                <p>{product.quantity}</p>
-                                                <button>+</button>
+                                            <div className='flex items-center justify-center border'>
+                                                <button className='text-xl font-bold px-1.5 border-r'>-</button>
+                                                <p className='text-xl px-2'>{product.quantity}</p>
+                                                <button className='text-xl px-1 border-1'>+</button>
                                             </div>
                                             <p>${(product.quantity * product.price).toFixed(2)}</p>
-                                            <button>
+                                            <button className='text-red-500 hover:text-red-700'>
                                                 <FaTrashAlt />
                                             </button>
                                         </div>
