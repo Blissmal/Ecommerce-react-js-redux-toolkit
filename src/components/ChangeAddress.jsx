32b64c1d701f0a35ca45871fb8c2a953c4c24723
Blissmal <malutibethuel@gmail.com>
2025-02-11 23:52:21 +0300

    Added styling

--- src/components/ChangeAddress.jsx
+++ src/components/ChangeAddress.jsx
@@ -4,8 +4,10 @@ const ChangeAddress = () => {
   return (
     <div>
       <input type="text" placeholder='Enter new address' className='border p-2 w-full mb-4'/>
-      <button>Cancel</button>
-      <button>Save Address</button>
+      <div className='flex justify-end'>
+      <button className='bg-gray-500 text-white py-2 px-4 rounded mr-2'>Cancel</button>
+      <button className='bg-blue-500 text-white py-2 rounded'>Save Address</button>
+      </div>
     </div>
   )
 }
