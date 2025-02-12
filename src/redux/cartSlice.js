7a810a7ceb719feb98d315ab7d27d543c518d0d4
Blissmal <malutibethuel@gmail.com>
2025-02-12 12:04:54 +0300

    Added increase and decrease quantities

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -13,8 +13,8 @@ const cartSlice = createSlice({
         addToCart(state, action) {
             const newItem = action.payload;
             const itemIndex = state.products.find(item => item.id === newItem.id) //finding index
-            if (itemIndex){
-                itemIndex.quantity ++;
+            if (itemIndex) {
+                itemIndex.quantity++;
                 itemIndex.totalPrice += newItem.price
             } else {
                 state.products.push({
@@ -27,9 +27,9 @@ const cartSlice = createSlice({
                 })
             }
             state.totalPrice += newItem.price
-            state.totalQuantity ++
+            state.totalQuantity++
         },
-        removeFromCart(state, action){
+        removeFromCart(state, action) {
             const id = action.payload;
             const findItem = state.products.find(item => item.id === id)
             if (findItem) {
@@ -37,9 +37,29 @@ const cartSlice = createSlice({
                 state.totalQuantity -= findItem.quantity
                 state.products = state.products.filter(item => item.id !== id)
             }
-        }
+        },
+        increaseQuantity(state, action) {
+            const id = action.payload;
+            const findItem = state.products.find(item => item.id === id)
+            if (findItem) {
+                findItem.quantity++;
+                findItem.totalPrice += findItem.price;
+                state.totalQuantity++;
+                state.totalPrice += findItem.price
+            }
+        },
+        decreaseQuantity(state, action) {
+            const id = action.payload;
+            const findItem = state.products.find(item => item.id === id)
+            if (findItem) {
+                findItem.quantity--;
+                findItem.totalPrice -= findItem.price;
+                state.totalQuantity--;
+                state.totalPrice -= findItem.price
+            }
+        },
     },
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions
 export default cartSlice.reducer
