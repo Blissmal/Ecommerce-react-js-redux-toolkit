c1187537b0c61df2f34a842377afeb2e66707d57
Blissmal <malutibethuel@gmail.com>
2025-02-12 19:33:24 +0300

    Product card changeover to new ui

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,104 @@
-import React from 'react'
-import { FaStar } from 'react-icons/fa'
-import { useDispatch } from 'react-redux'
-import { addToCart } from '../redux/cartSlice'
+import { FaStar } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
 
-const ProductCard = ({product}) => {
-  const dispatch = useDispatch()
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
   const handleAddToCart = (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    dispatch(addToCart(product))
-    alert("product Added succesfully!")
-  }
+    e.stopPropagation();
+    e.preventDefault();
+    dispatch(addToCart(product));
+    alert("product Added succesfully!");
+  };
   return (
-    <div className='bg-white p-4 shadow rounded relative border transform transition-transform duration-300 hover:scale-105'>
-      <img className='w-full h-48 object-contain mb-4' src={product.image} alt="" />
-      <h3 className='text-lg font-semibold'>{product.name}</h3>
-      <p className='text-gray-500'>${product.price}</p>
-      <div className='flex items-center mt-2'>
-        <FaStar className='text-yellow-500'/>
-        <FaStar className='text-yellow-500'/>
-        <FaStar className='text-yellow-500'/>
-        <FaStar className='text-yellow-500'/>
+    <div
+      key={product.id}
+      className="space-y-6 overflow-hidden rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800"
+    >
+      <a href="#" className="overflow-hidden rounded">
+        <img
+          className="mx-auto h-44 w-44 rounded-lg mb-3"
+          src={product.image}
+          alt="imac image"
+        />
+      </a>
+      <div>
+        <a
+          href="#"
+          className="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white"
+        >
+          {product.name}
+        </a>
+        <p className="mt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+          This generation has some improvements, including a longer continuous
+          battery life.
+        </p>
       </div>
-      <div className='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all' onClick={(e) => handleAddToCart(e, product)}>
-        <span className='group-hover:hidden'>+</span>
-        <span className='hidden group-hover:block'>Add To Cart</span>
+      <div>
+        <p className="text-lg font-bold text-gray-900 dark:text-white">
+          <span className="line-through"> ${product.price} </span>
+        </p>
+        <p className="text-lg font-bold leading-tight text-red-600 dark:text-red-500">
+          $9.99
+        </p>
+      </div>
+      <div className="mt-6 flex items-center gap-2.5">
+        <button
+          data-tooltip-target="favourites-tooltip-1"
+          type="button"
+          className="inline-flex items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white p-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+        >
+          <svg
+            className="h-5 w-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z"
+            ></path>
+          </svg>
+        </button>
+        <div
+          id="favourites-tooltip-1"
+          role="tooltip"
+          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+        >
+          Add to favourites
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
+        <button
+          type="button"
+          onClick={e => handleAddToCart(e, product)}
+          className="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium  text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          <svg
+            className="-ms-2 me-2 h-5 w-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4"
+            />
+          </svg>
+          Add to cart
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
