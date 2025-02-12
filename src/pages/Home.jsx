81664dbf062e5b86708179564d695a504f594602
Blissmal <malutibethuel@gmail.com>
2025-02-12 19:35:06 +0300

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   const products = useSelector((state) => state.product);
   useEffect(() => {
     dispatch(setProducts(mockData));
-  }, []);
+  }, [dispatch]);
   return (
     <div>
       <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
