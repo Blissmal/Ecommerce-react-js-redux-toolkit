b224fa30c0618bea3ca1dc309dbc309878cb28e0
Blissmal <malutibethuel@gmail.com>
2025-02-15 00:35:38 +0300

    code edits

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -8,6 +8,12 @@ const Checkout = () => {
     const [paymentToggle, setpaymentToggle] = useState(false)
 
     const [paymentMethod, setpaymentMethod] = useState("CoD")
+    const [shippingInfo, setshippingInfo] = useState({
+        address: '',
+        city: '',
+        zip: ''
+    })
+
     const cart = useSelector(state => state.cart)
     if (cart.products.length > 0) return (
         <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
