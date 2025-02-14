dfb78982ba11874ddf7fd15889bd720fa703e853
Blissmal <malutibethuel@gmail.com>
2025-02-15 01:18:56 +0300

    Added tailwind class styling

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -2,36 +2,36 @@ import React from 'react'
 
 const Order = ({order}) => {
   return (
-    <div>
-      <h2>Thank you for your Order</h2>
+    <div className='container mx-auto py-8 px-4 md:px-16 lg:px-24'>
+      <h2 className='text-2xl font-semibold mb-4'>Thank you for your Order</h2>
       <p>Your order has been placed</p>
-      <div>
-        <h3>Order Summary</h3>
+      <div className='mt-6 p-4 border rounded-lg bg-gray-100'>
+        <h3 className='text-lg font-semibold mb-2'>Order Summary</h3>
         <p>Order Nummber: {order.orderNumber}</p>
-        <div>
-            <h2>Shipping Information</h2>
+        <div className='mt-4'>
+            <h4 className='text-md font-semibold mb-2'>Shipping Information</h4>
             <p>{order.shippingInformation.address}</p>
             <p>{order.shippingInformation.city}</p>
             <p>{order.shippingInformation.zip}</p>
         </div>
-        <div>
-            <h3>Products Ordered</h3>
+        <div className='mt-4'>
+            <h4 className='text-md font-semibold mb-2'>Products Ordered</h4>
             {
-                order.products.map((product, index) => (
-                    <div key={index}>
+                order.products.map((product) => (
+                    <div key={product.id} className='flex justify-between mt-2'>
                         <p>{product.name} x {product.quantity}</p>
                         <p>{product.price * product.quantity}</p>
                     </div>
                 ))
             }
         </div>
-        <div>
+        <div className='mt-4 flex justify-between'>
             <span>Total Price: </span>
-            <span>{order.totalPrice}</span>
+            <span className='font-semibold'>${order.totalPrice.toFixed(2)}</span>
         </div>
-        <div>
-            <button>Order tracking</button>
-            <button>Continue Shopping</button>
+        <div className='mt-6'>
+            <button className='bg-green-500 text-white py-2 px-4 hover:bg-green-600'>Order tracking</button>
+            <button className='ml-4 bg-red-700600 text-white py-2 px-4 bg-red-500 hover:bg-red-800'>Continue Shopping</button>
         </div>
       </div>
     </div>
