86cf948491a91774b7b6a32bf7c29a8f5636f4fe
Blissmal <malutibethuel@gmail.com>
2025-02-15 01:08:29 +0300

    Added Order page elements

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -8,6 +8,31 @@ const Order = ({order}) => {
       <div>
         <h3>Order Summary</h3>
         <p>Order Nummber: {order.orderNumber}</p>
+        <div>
+            <h2>Shipping Information</h2>
+            <p>{order.shippingInformation.address}</p>
+            <p>{order.shippingInformation.city}</p>
+            <p>{order.shippingInformation.zip}</p>
+        </div>
+        <div>
+            <h3>Products Ordered</h3>
+            {
+                order.products.map((product, index) => (
+                    <div key={index}>
+                        <p>{product.name} x {product.quantity}</p>
+                        <p>{product.price * product.quantity}</p>
+                    </div>
+                ))
+            }
+        </div>
+        <div>
+            <span>Total Price: </span>
+            <span>{order.totalPrice}</span>
+        </div>
+        <div>
+            <button>Order tracking</button>
+            <button>Continue Shopping</button>
+        </div>
       </div>
     </div>
   )
