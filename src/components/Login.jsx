965c2a3e6994a3c91bb1a6640cea4e33ee3b8535
Blissmal <malutibethuel@gmail.com>
2025-02-15 23:33:01 +0300

    Added login elements

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      
+      <h2>Login</h2>
+      <form action="">
+        <div>
+            <label htmlFor="">Email</label>
+            <input type="email" />
+        </div>
+
+        <div>
+            <label htmlFor="">Password</label>
+            <input type="password" />
+        </div>
+
+        <div>
+            <label htmlFor="">
+                <input type="checkbox" />
+                <span>Remember Me</span>
+                <a href="">Forgot Password ?</a>
+            </label>
+        </div>
+
+        <div>
+            <button>Login</button>
+        </div>
+      </form>
+      <div>
+        <span>Don&#39;t have any account ?</span>
+        <button>Sign Up</button>
+      </div>
     </div>
   )
 }
