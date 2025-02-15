e5c3d661a7f3ccf2d450c139dd0cb4a3492bdd30
Blissmal <malutibethuel@gmail.com>
2025-02-15 23:39:10 +0300

    Added styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,33 +3,33 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <h2>Login</h2>
+      <h2 className='text-2xl font-bold mb-4'>Login</h2>
       <form action="">
-        <div>
-            <label htmlFor="">Email</label>
-            <input type="email" />
+        <div className='mb-4'>
+            <label htmlFor="" className='block text-gray-700'>Email</label>
+            <input type="email" className='w-full px-3 py-2 border' placeholder='Enter Email'/>
         </div>
 
-        <div>
-            <label htmlFor="">Password</label>
-            <input type="password" />
+        <div className='mb-4'>
+            <label htmlFor="" className='block text-gray-700'>Password</label>
+            <input type="password" className='w-full px-3 py-2 border' placeholder='Enter password'/>
         </div>
 
-        <div>
-            <label htmlFor="">
-                <input type="checkbox" />
-                <span>Remember Me</span>
-                <a href="">Forgot Password ?</a>
+        <div className='mb-4 flex items-center justify-between'>
+            <label htmlFor="" className='inline-flex items-center'>
+                <input type="checkbox" className='form-checkbox'/>
+                <span className='ml-2 text-gray-700'>Remember Me</span>
+                <a href="#" className='text-red-800'>Forgot Password ?</a>
             </label>
         </div>
 
-        <div>
-            <button>Login</button>
+        <div className='mb-4'>
+            <button className='w-full bg-red-600 text-white py-2' type='submit'>Login</button>
         </div>
       </form>
-      <div>
-        <span>Don&#39;t have any account ?</span>
-        <button>Sign Up</button>
+      <div className='text-center'>
+        <span className='text-gray-700'>Don&#39;t have any account ?</span>
+        <button className='text-red-800'>Sign Up</button>
       </div>
     </div>
   )
