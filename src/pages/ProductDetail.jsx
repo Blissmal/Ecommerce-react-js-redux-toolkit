209a3a6232e0cc12385e0423f01f9e98d41b75c0
Blissmal <malutibethuel@gmail.com>
2025-02-16 00:48:10 +0300

    Product search fetch and assign to a state variable using useEffect hook

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 const ProductDetail = () => {
+    const {id} = useParams()
+    const products = useSelector(state => state.product.products)
+    const [product, setProduct] = useState()
+
+    useEffect(() => {
+        const newProduct = products.find(product => product.id === id)
+        setProduct(newProduct)
+    }, [id, products])
   return (
     <div>
       
