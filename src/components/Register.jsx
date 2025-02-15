38ccad7bb135aeb660403dd5981e7f525e165d1a
Blissmal <malutibethuel@gmail.com>
2025-02-15 23:42:35 +0300

    Register part

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,13 @@
 const Register = () => {
   return (
     <div>
-      <h2 className='text-2xl font-bold mb-4'>Login</h2>
+      <h2 className='text-2xl font-bold mb-4'>Sign Up</h2>
       <form action="">
+      <div className='mb-4'>
+            <label htmlFor="" className='block text-gray-700'>Name</label>
+            <input type="text" className='w-full px-3 py-2 border' placeholder='Enter Name'/>
+        </div>
+
         <div className='mb-4'>
             <label htmlFor="" className='block text-gray-700'>Email</label>
             <input type="email" className='w-full px-3 py-2 border' placeholder='Enter Email'/>
@@ -13,20 +18,12 @@ const Register = () => {
             <input type="password" className='w-full px-3 py-2 border' placeholder='Enter password'/>
         </div>
 
-        <div className='mb-4 flex items-center justify-between'>
-            <label htmlFor="" className='inline-flex items-center'>
-                <input type="checkbox" className='form-checkbox'/>
-                <span className='ml-2 text-gray-700'>Remember Me</span>
-                <a href="#" className='text-red-800'>Forgot Password ?</a>
-            </label>
-        </div>
-
         <div className='mb-4'>
-            <button className='w-full bg-red-600 text-white py-2' type='submit'>Login</button>
+            <button className='w-full bg-red-600 text-white py-2' type='submit'>Sign Up</button>
         </div>
       </form>
       <div className='text-center'>
-        <span className='text-gray-700'>Don&#39;t have any account ?</span>
+        <span className='text-gray-700'>ALready have an account ?</span>
         <button className='text-red-800'>Sign Up</button>
       </div>
     </div>
