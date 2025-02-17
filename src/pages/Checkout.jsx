033a660a07894dd8055d4bded470ae24639b9b9a
Blissmal <malutibethuel@gmail.com>
2025-02-17 21:42:45 +0300

    handleOrder function added

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -609,7 +609,7 @@ const Checkout = ({setOrder}) => {
                         </div>
 
                         <div className="space-y-3">
-                            <button type="submit" className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Proceed to Payment</button>
+                            <button type="submit" onClick={handleOrder} className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Proceed to Payment</button>
 
                             <p className="text-sm font-normal text-gray-500 dark:text-gray-400">One or more items in your cart require an account. <a href="#" title="" className="font-medium text-primary-700 underline hover:no-underline dark:text-primary-500">Sign in or create an account now.</a>.</p>
                         </div>
