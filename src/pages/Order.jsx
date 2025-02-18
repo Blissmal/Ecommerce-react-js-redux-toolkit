d14a2b806176acded193b3e1a27add9ee4015f79
Blissmal <malutibethuel@gmail.com>
2025-02-18 23:44:51 +0300

    Removed unused code

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -7,46 +7,6 @@ const Order = ({ order }) => {
   const cart = useSelector((state) => state.cart);
   if (cart.products.length > 0)
   return (
-    // <div classNameName="container mx-auto py-8 px-4 md:px-16 lg:px-24">
-    //   <h2 classNameName="text-2xl font-semibold mb-4">Thank you for your Order</h2>
-    //   <p>Your order has been placed</p>
-    //   <div classNameName="mt-6 p-4 border rounded-lg bg-gray-100">
-    //     <h3 classNameName="text-lg font-semibold mb-2">Order Summary</h3>
-    //     <p>Order Nummber: {order.orderNumber}</p>
-    //     <div classNameName="mt-4">
-    //       <h4 classNameName="text-md font-semibold mb-2">Shipping Information</h4>
-    //       <p>{order.shippingInformation.name}</p>
-    //       <p>{order.shippingInformation.country}</p>
-    //       <p>{order.shippingInformation.city}</p>
-    //     </div>
-    //     <div classNameName="mt-4">
-    //       <h4 classNameName="text-md font-semibold mb-2">Products Ordered</h4>
-    //       {order.products.map((product) => (
-    //         <div key={product.id} classNameName="flex justify-between mt-2">
-    //           <p>
-    //             {product.name} x {product.quantity}
-    //           </p>
-    //           <p>{product.price * product.quantity}</p>
-    //         </div>
-    //       ))}
-    //     </div>
-    //     <div classNameName="mt-4 flex justify-between">
-    //       <span>Total Price: </span>
-    //       <span classNameName="font-semibold">${order.totalPrice.toFixed(2)}</span>
-    //     </div>
-    //     <div classNameName="mt-6">
-    //       <button classNameName="bg-green-500 text-white py-2 px-4 hover:bg-green-600">
-    //         Order tracking
-    //       </button>
-    //       <button
-    //         classNameName="ml-4 bg-red-700600 text-white py-2 px-4 bg-red-500 hover:bg-red-800"
-    //         onClick={() => navigate("/")}
-    //       >
-    //         Continue Shopping
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
 <div className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
 
   <div className="flex justify-start item-start space-y-2 flex-col">
