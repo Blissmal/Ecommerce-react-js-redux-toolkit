cb66ecaa2e62e30841b5101cfdae6839737ebd2b
Blissmal <malutibethuel@gmail.com>
2025-03-08 00:43:13 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import { useState } from 'react'
-import Order from './pages/Order'
-import FilterData from './pages/FilterData'
-import ProductDetail from './pages/ProductDetail'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import { useState } from "react";
+import Order from "./pages/Order";
+import FilterData from "./pages/FilterData";
+import ProductDetail from "./pages/ProductDetail";
 
 const App = () => {
-  const [order, setOrder] = useState(null)
+  const [order, setOrder] = useState(null);
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/shop' element={<Shop />}/>
-        <Route path='/cart' element={<Cart />}/>
-        <Route path='/checkout' element={<Checkout setOrder={setOrder}/>}/>
-        <Route path='/order-confirmation' element={<Order order={order}/>}/>
-        <Route path='/filter-data' element={<FilterData />}/>
-        <Route path='/product/:id' element={<ProductDetail />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout setOrder={setOrder} />} />
+        <Route path="/order-confirmation" element={<Order order={order} />} />
+        <Route path="/filter-data" element={<FilterData />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
