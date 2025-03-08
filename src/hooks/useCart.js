416ad80439e6bc6ad9e8e4ab250de255df21edbc
Blissmal <malutibethuel@gmail.com>
2025-03-09 01:23:49 +0300

    Added a custom hook useCart

---
+++ src/hooks/useCart.js
@@ -0,0 +1,17 @@
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+import { useState } from "react";
+
+export const useCart = () => {
+  const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddToCart = (e, product) => {
+    e.stopPropagation()
+    e.preventDefault()
+    dispatch(addToCart(product));
+    setIsModalOpen(true);
+  };
+
+  return { handleAddToCart, isModalOpen, setIsModalOpen };
+};
